95422b2a96be87add0fd09accccc041593b59e38
binhaoliu <2843285389@qq.com>
2024-11-17 13:42:08 +0800

    update gots3

--- src/GitTe.java
+++ src/GitTe.java
@@ -3,5 +3,6 @@ public class GitTe {
         System.out.println("Hello World");
         System.out.println("Hello hots1");
         System.out.println("Hello hots1");
+        System.out.println("master test");
     }
 }
