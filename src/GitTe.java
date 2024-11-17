9f0719d6ad9963eba466cf632092c0abda8478f0
binhaoliu <2843285389@qq.com>
2024-11-17 13:21:47 +0800

    update 222

---
+++ src/GitTe.java
@@ -0,0 +1,7 @@
+public class GitTe {
+    public static void main(String[] args) {
+        System.out.println("Hello World");
+        System.out.printf("111");
+        System.out.printf("222");
+    }
+}
